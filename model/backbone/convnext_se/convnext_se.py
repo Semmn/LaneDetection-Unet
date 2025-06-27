update some code: 1. update learning rate scheduler settings, 2. fix some type error, 3. update lane mask to binary mask, 4. add automatic mixed precision

model/backbone/convnext_se/convnext_se.py
@@ -3,8 +3,6 @@ import torch
 import torch.nn as nn
 import torchvision
 
-
-
 def adjust_padding_for_strided_output(kernel_size, stride):
         # range of padding: (kernel_size - stride)/2 <= padding < (kernel_size)/2
         # this ensures that output convolution channel size are 1/(transition_stride) of the input size
@@ -346,4 +344,76 @@ class ConvNextV1CLS(torch.nn.Module):
         elif self.output_mode=='probs':
             return torch.nn.functional.softmax(x)
 
+# ConvNextV1REG
+class ConvNextV1REG(torch.nn.Module):
+    def __init__(self, backbone:ConvNextV1, device='cuda'):
+        super().__init__()
+        
+        self.backbone=backbone
+        self.device=device
+        
+        self.regressor_head = torch.nn.Linear(in_features=backbone.main_channels[-1], out_features=1, device=self.device)
+        
+    def forward(self, x):
+        x = self.backbone(x)
+        x = torch.nn.functional.avg_pool2d(x, x.size()[2:])
+        x = x.flatten(start_dim=1, end_dim=3)
+        return self.regressor_head(x) # output of regressor head is 1D vector (B, 1)
 
+# Convnext_se for detector backbone (lane point regression + bg/fg classification for detection task)
+class DetectorBackbone(torch.nn.Module):
+    def __init__(self, num_blocks:list, input_channels:int, stem_kersz:tuple, stem_stride:tuple, 
+                 img_hw:list, main_channels:list, expansion_dim:list, kernel_sz:list, stride:list, padding:list, dilation:list, groups:list, droprate:list, drop_mode:list, use_se:list, squeeze_ratio:int,
+                 transition_kersz:list, transition_stride:list, norm_mode:str, device='cuda'):
+        
+        super().__init__()
+        
+        self.num_blocks = num_blocks # number of blocks for each stage
+        self.input_channels = input_channels # mostly RGB 3 channel images
+        self.stem_kersz = stem_kersz # kernel size for stem layer
+        self.stem_stride = stem_stride # stride for stem layer
+        self.img_hw = img_hw # representative image height and width for each stage
+        self.main_channels = main_channels # main channels for each stage
+        self.expansion_dim = expansion_dim # expansion dimension for each stage
+        self.kernel_sz = kernel_sz # kernel size for each stage
+        self.stride=stride # stride for each stage
+        self.padding=padding # padding for each stage
+        self.dilation=dilation # dilation for each stage
+        self.groups=groups # number of groups for each stage
+        self.droprate=droprate # constant droprate for all stage
+        self.drop_mode=drop_mode # drop_mode is same for all stage
+        self.use_se=use_se # flag for using se operation in each stage
+        self.squeeze_ratio=squeeze_ratio # squeeze_ratio is same for all stage
+        self.transition_kersz=transition_kersz # transition kernel size for each stage
+        self.transition_stride=transition_stride # transition stride for each stage
+        
+        if norm_mode not in ['layer_norm', 'batch_norm']:
+            raise Exception(f"Unsupported normalization method: {norm_mode}. Must be either 'layer_norm' or 'batch_norm'")
+        self.norm_mode = norm_mode
+        
+        self.device=device
+        self.num_stages = len(num_blocks)
+        
+        self.stem = Stem(stem_in_channels=self.input_channels, stem_out_channels=main_channels[0], stem_kernel_sz=self.stem_kersz, stem_stride=self.stem_stride,
+                         stem_padding=adjust_padding_for_strided_output(self.stem_kersz, self.stem_stride), stem_dilation=1, stem_groups=1, device=self.device)
+        self.stages = []
+        for i in range(self.num_stages):
+            if i == 0:
+                self.stages.append(Stage(transition_flag=False, num_blocks=self.num_blocks[i], img_hw=self.img_hw[i], in_channels=self.main_channels[i], out_channels=self.expansion_dim[i],
+                                    kernel_sz=self.kernel_sz[i], stride=self.stride[i], padding=self.padding[i], dilation=self.dilation[i], groups=self.groups[i], droprate=self.droprate[i],
+                                    drop_mode=self.drop_mode[i], use_se=self.use_se[i], squeeze_ratio=self.squeeze_ratio, norm_mode=self.norm_mode, device='cuda'))
+            else:
+                self.stages.append(Stage(transition_flag=True, num_blocks=self.num_blocks[i], img_hw=self.img_hw[i], in_channels=self.main_channels[i], out_channels=self.expansion_dim[i],
+                                        kernel_sz=self.kernel_sz[i], stride=self.stride[i], padding=self.padding[i], dilation=self.dilation[i], groups=self.groups[i], droprate=self.droprate[i],
+                                        drop_mode=self.drop_mode[i], use_se=self.use_se[i], squeeze_ratio=self.squeeze_ratio, transition_channels=self.main_channels[i-1], transition_kersz=self.transition_kersz[i],
+                                        transition_stride=self.transition_stride[i], norm_mode=self.norm_mode, device='cuda'))
+        self.stages = torch.nn.ModuleList(self.stages)
+    
+    # as same as u-net encoder design, each stage outputs final block output in each stage
+    def forward(self, x):
+        stage_output = []
+        x = self.stem(x)
+        for i in range(self.num_stages):
+            x = self.stages[i](x)
+            stage_output.append(x)
+        return stage_output

train/segmentation/dataset.py
@@ -9,7 +9,7 @@ import torch
 # num_classes should include background class. you can decide whether to exclude background class in label mask or not
 # remember that mask_onehot and image shape must be same. if you exclude background class in mask, your model should output num_classes-1 channels
 class CULaneSegDataset(torch.utils.data.Dataset):
-    def __init__(self, segmentation_img_list, segmentation_mask_list, transforms=None, num_classes=5, is_test=False):
+    def __init__(self, segmentation_img_list, segmentation_mask_list, transforms=None, num_classes=2, is_test=False):
         self.segmentation_img_list = segmentation_img_list
         self.segmentation_mask_list = segmentation_mask_list
         self.transforms = transforms # transforms that are only related to source image
@@ -46,6 +46,10 @@ class CULaneSegDataset(torch.utils.data.Dataset):
                 transformed = self.transforms(image=image, mask=mask)
                 image = transformed['image']
                 mask = transformed['mask']
+            
+            if self.num_classes == 2:
+                mask = torch.where(mask > 0, 1, 0) # convert mask to binary (0 or 1)
+                
             mask_onehot = torch.nn.functional.one_hot(mask.long(), num_classes=self.num_classes)
             mask_onehot = mask_onehot.permute(2,0,1) # (H,W,C) -> (C, H, W)
             return image, mask_onehot

train/segmentation/deepspeed.py
@@ -1,13 +1,14 @@
-# append python search path
 import sys, os
 
-sys.path.append('/work') # append work directory
+sys.path.append('/work') 
+
 from model.head.convnext_se_unet import Encoder, DBlock, DStage, DStemNx, DStemStacked, DStemStaged, Decoder, UNet
 from model.utils.conv_2d import adjust_padding_for_strided_output, DepthWiseSepConv
 from model.utils.stochastic_depth_drop import create_linear_p, create_uniform_p
 
 from train.utils.loss import DICELoss, LaneDICELoss
 from train.utils.metrics import get_dice_score, get_iou_score, get_lane_score
+from train.utils.logger import checkdir
 
 from dataset import CULaneSegDataset
 
@@ -31,44 +32,28 @@ from albumentations.pytorch import ToTensorV2
 # for ConsineSchedulerLR
 import timm.scheduler
 
-# for pytorch distributed training
-from torch.utils.data.distributed import DistributedSampler
-import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel as DDP
-
 # for using deepspeed library
 import deepspeed
 from deepspeed.accelerator import get_accelerator
 
-
 def add_argument():
-    parser = argparse.ArgumentParser(description='CULane')
+    parser = argparse.ArgumentParser(description='CULane DeepSpeed Training')
     
     parser.add_argument('--val_batch_size', default=32, type=int, help='batch size for validation')
     parser.add_argument('--epoch', type=int, default=100, help='total number of epochs for training')
     parser.add_argument('--start_epoch', type=int, default=0, help='start epoch of training')
     parser.add_argument('--local-rank', type=int, default=0, help='local rank of the process')
-    parser.add_argument('--rank', type=int, default=0, help='rank of the process')
     parser.add_argument('--ds_config_path', type=str, default='/work/train/CULane/ds_config.json', help='deepspeed configuration json file path')
-    
-    parser.add_argument('--project_name', type=str, default=f'convnext_tiny_unet/culane', help='project name used for defining save path')
+    parser.add_argument('--project_name', type=str, default='convnext_unet/culane/deepspeed', help='project name used for defining save path')
     parser.add_argument('--weight_dir', type=str, default='/work/checkpoints', help='basic path for saving model weights and files')
     parser.add_argument('--plot_dir', type=str, default='/work/plots', help='basic path for saving plots')
     parser.add_argument('--save_step', type=int, default=10, help='steps for model saving')
     parser.add_argument('--save_plot_step', type=int, default=5, help='steps for plot saving')
     
-    parser.add_argument('--ckpt_id', type=str, help='checkpoint id for loading model. in this case, use saved epoch number')
-    
     parser = deepspeed.add_config_arguments(parser)
     args = parser.parse_args()
     
     return args
-
-# check whether the directory exists or not
-def checkdir(path):
-    if not os.path.exists(path):
-        print("creating directories: ", path)
-        os.makedirs(path)
         
 def save_plots(train_history, val_history, train_dice, val_dice, train_lane, val_lane, epoch, save_plot_path):
     checkdir(save_plot_path)
@@ -130,10 +115,10 @@ def get_ds_config(args):
 
 def main(opts):
     
-    deepspeed.init_distributed()
     print("Distributed training Initialized")
-    _local_rank = int(os.environ.get("LOCAL_RANK"))
-    get_accelerator().set_device(_local_rank)
+    deepspeed.init_distributed() # initialize deepspeed distribute environment instead of pytorch distributed training setup
+    local_rank = int(os.environ.get("LOCAL_RANK"))
+    get_accelerator().set_device(local_rank)
     
     if opts.rank == 0:
         currtime = (datetime.datetime.utcnow() + datetime.timedelta(hours=9)).strftime("%Y_%m_%d_%H%M%S") # need to plus 9 hours to utc time (ktz=utc+9)
@@ -234,21 +219,13 @@ def main(opts):
     # 3) deepspeed optimizer
     ds_config = get_ds_config(opts)
     parameters = filter(lambda p: p.requires_grad, convnext_unet.parameters())
-    model_engine, optimizer, train_dataloader, _ = deepspeed.initialize(args=opts, model=convnext_unet, model_parameters=parameters, training_data=train_dataset, config=ds_config,)
-    
-    if opts.start_epoch != 0: # if start epoch of training is not zero
-        # load checkpoints
-        _, client_sd = model_engine.load_checkpoint(opts.save_path + '/weights', opts.ckpt_id) # ckpt_id is epoch (1-index ordered)
-        epoch = client_sd['epoch']
-        if opts.start_epoch != epoch: # opts.start_epoch should also be 1-index ordered.
-            raise Exception("saved epoch and opts.start_epoch are not matched!")
-    else:
-        client_sd = {}
-        
+    model_engine, optimizer, train_dataloader, _ = deepspeed.initialize(args=opts, model=convnext_unet, model_parameters=parameters,
+                                                                        training_data=train_dataset, config=ds_config)
     #get the local device name(str) and local rank (int)
     local_device = get_accelerator().device_name(model_engine.local_rank)
     local_rank = model_engine.local_rank
     
+    # for float32, target_dtype will be None so no datatype conversion needed.
     target_dtype = None
     if model_engine.bfloat16_enabled():
         target_dtype = torch.bfloat16
@@ -259,7 +236,7 @@ def main(opts):
     # criterion = DICELoss(weights=[], num_cls=5) for weighted Dice Loss
     # criterion = DICELoss(num_cls=4) # custom dice loss for segmentation tasks
     criterion = LaneDICELoss(num_cls=5) # background information will be treated as 0-th channel
-
+            
     train_history = []
     val_history = []
 
@@ -302,9 +279,8 @@ def main(opts):
             loss_container.append(loss.detach().item())
             
             dice_coeff = get_dice_score(pred_mask.cpu().detach().numpy(), mask.cpu().detach().numpy())
-            dice_container.append(dice_coeff)
-            
             lane_coeff = get_lane_score(pred_mask.cpu().detach().numpy(), mask.cpu().detach().numpy())
+            dice_container.append(dice_coeff)
             lane_container.append(lane_coeff)
             
             running_loss += loss.detach().item()
@@ -348,13 +324,13 @@ def main(opts):
                 loss_container.append(loss.detach().item())
                 
                 dice_coeff = get_dice_score(pred_mask.cpu().detach().numpy(), mask.cpu().detach().numpy())
-                dice_container.append(dice_coeff)
-                
                 lane_coeff = get_lane_score(pred_mask.cpu().detach().numpy(), mask.cpu().detach().numpy())
+                dice_container.append(dice_coeff)
                 lane_container.append(lane_coeff)
                 
                 running_loss += loss.detach().item()
-                running_lane += lane_coeff 
+                running_lane += lane_coeff
+                
                 if local_rank == 0 and i % val_print_threshold == 0:
                     print(f"Validation [{epoch+1}:{i:5d}/{total_val_iter}], loss: {running_loss/val_print_threshold:.3f}, lane-dice: {running_lane/print_threshold:.3f}")
                     running_loss = 0.0
@@ -373,14 +349,14 @@ def main(opts):
             print("="*50)
             checkdir(opts.save_path+'/weights')
             
-            if ((epoch+1) % opts.save_step) == 0 or (epoch+1)==opts.epoch:
-                print("+"*50)
-                print(f"Saving Model to {opts.save_path}/weights...")
-                print("+"*50)
+            # if ((epoch+1) % opts.save_step) == 0 or (epoch+1)==opts.epoch:
+            #     print("+"*50)
+            #     print(f"Saving Model to {opts.save_path}/weights...")
+            #     print("+"*50)
                 
-                client_sd['epoch'] = epoch
-                ckpt_id = epoch+1 # checkpoint id is epoch following 1-index ordering
-                model_engine.save_checkpoint(opts.save_path+'/weights', ckpt_id, client_sd=client_sd)
+            #     client_sd['epoch'] = epoch
+            #     ckpt_id = epoch+1 # checkpoint id is epoch following 1-index ordering
+            #     model_engine.save_checkpoint(opts.save_path+'/weights', ckpt_id, client_sd=client_sd)
                 
             if ((epoch+1) % opts.save_plot_step) == 0 or (epoch+1)==opts.epoch:
                 save_plots(train_history, val_history, train_dice, val_dice, train_lane, val_lane, epoch+1, opts.save_plot_path) # epochs as 1-index ordering

train/segmentation/multigpu.py
@@ -2,8 +2,9 @@ import sys
 sys.path.append('/work')
 
 from train.segmentation.dataset import CULaneSegDataset
-
 from train.utils.loss import DICELoss, LaneDICELoss
+from train.utils.recorder import Recorder
+
 from train.utils.metrics import get_dice_score, get_iou_score, get_lane_score
 from train.utils.weight_load import load_weight, load_weight_with_cp, set_weights
 from train.utils.logger import set_logger_for_training, checkdir
@@ -12,7 +13,6 @@ from train.utils.plotting import plot_figure
 from train.utils.dataset import read_culane_segdata
 from train.utils.create_model import create_unet
 from train.utils.utils import copy_files
-from train.utils.recorder import Recorder
 
 import numpy as np
 import argparse
@@ -41,6 +41,10 @@ import timm.scheduler
 from torch.utils.data.distributed import DistributedSampler
 import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel as DDP
+
+# for pytorch mixed precision training
+from torch import autocast
+from torch.amp import GradScaler
 
 
 def get_args_parser():
@@ -144,6 +148,7 @@ def main(opts):
                                                  num_workers=int(opts.num_workers/opts.world_size),
                                                  sampler=val_sampler,
                                                  pin_memory=False)
+    
     # test_dataloader = torch.utils.data.DataLoader(test_dataset,
     #                                               batch_size=opts.val_batch_size,
     #                                               shuffle=False,
@@ -151,8 +156,10 @@ def main(opts):
     #                                               sampler=test_sampler,
     #                                               pin_memory=True)
     
-
     convnext_unet, criterion, optimizer, scheduler, start_epoch, end_epoch, scheduler_type = set_weights(opts, convnext_unet, train_dataloader, process_logger)
+    
+    # creates a GradScaler for mixed precision training
+    scaler = GradScaler()
             
     recorder = Recorder()
     recorder.register(['train_loss', 'train_dice', 'train_lane', 'val_loss', 'val_dice', 'val_lane']) # register each metric name for recording
@@ -183,11 +190,15 @@ def main(opts):
             
             optimizer.zero_grad()
             
-            prediction_mask = convnext_unet(image)
+            # mixed precision training
+            with autocast(device_type='cuda', dtype=torch.float16):
+                prediction_mask = convnext_unet(image)        
+                loss = criterion(prediction_mask, mask)
             
-            loss = criterion(prediction_mask, mask)
-            loss.backward()
-            optimizer.step()
+            # scale loss.  calls backward() on the scaled loss to create scaled gradients
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update() # update the scaler
             
             dice_score = get_dice_score(prediction_mask.cpu().detach().numpy(), mask.cpu().detach().numpy())
             lane_score = get_lane_score(prediction_mask.cpu().detach().numpy(), mask.cpu().detach().numpy())

train/unsupervised/pixel_reconstruction/multigpu_self_supervised.py
@@ -41,6 +41,10 @@ from torch.utils.data.distributed import DistributedSampler
 import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel as DDP
 
+# for pytorch automatic mixed precision training
+from torch import autocast
+from torch.amp import GradScaler
+
 # set argument parser
 def get_args_parser():
     parser = argparse.ArgumentParser(add_help=False)
@@ -136,7 +140,10 @@ def main(opts):
                                                 pin_memory=False)
     
     convnext_unet, criterion, optimizer, scheduler, start_epoch, end_epoch, scheduler_type = set_weights(opts, convnext_unet, train_dataloader, process_logger)
-        
+    
+    # creates a GradScaler for mixed precision training
+    scaler = GradScaler()
+    
     recorder = Recorder()
     recorder.register(['train_loss', 'train_pixel_reconst', 'val_loss', 'val_pixel_reconst']) # register each metric name for recording
     recorder.register_scalar(['running_loss', 'running_pixel_reconst']) # register temporal scalar value for recording
@@ -179,16 +186,20 @@ def main(opts):
             mask_location = (image==0).all(dim=1) # (B, C, H, W) -> channel-wisely when all pixel-values are zero, the pixel is masked.
             
             optimizer.zero_grad()
+
+            # mixed precision training
+            with autocast(device_type='cuda', dtype=torch.float16):
+                prediction_mask = convnext_unet(image)
             
-            prediction_mask = convnext_unet(image)
+                if opts.loss_type == 'mse':
+                    loss = criterion(prediction_mask, mask) # when use torch.nn.mse_loss()
+                elif opts.loss_type =='masked_mse':
+                    loss = criterion(prediction_mask, mask, mask_location) # when use PixelReconstructLoss()
             
-            if opts.loss_type == 'mse':
-                loss = criterion(prediction_mask, mask) # when use torch.nn.mse_loss()
-            elif opts.loss_type =='masked_mse':
-                loss = criterion(prediction_mask, mask, mask_location) # when use PixelReconstructLoss()
-            
-            loss.backward()
-            optimizer.step()
+            # scale loss. calls backward() on the scaled loss to create scaled gradients
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update() # update the scaler
             
             if opts.loss_type == 'mse':
                 pixel_reconst = get_masked_pixel_reconst(prediction_mask, mask, mask_location)

train/utils/weight_load.py
@@ -272,7 +272,7 @@ def set_weights(opts, model, dataloader, logger):
             last_epoch = -1 # start from scratch
         milestones = scheduler_config['milestones'] # period of learning rate decay
         gamma = scheduler_config['gamma'] # multiplicative factor of learning rate decay
-        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, step_size=milestones, gamma=gamma)
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones, gamma=gamma)
     else:
         raise Exception(f"Not Supported Scheduler: {scheduler_type} Currently learning rate scheduler must be one of 'cosine_warmup_restart', 'one_cycle', 'multi_step")
     end_epoch = scheduler_config['epoch'] # scheduler container 'epoch'